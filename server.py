f385648db984d63bd896db2b4245e38cf9d0749f
Hadar Elkayam <hadarel4@gmail.com>
2025-01-14 03:37:57 +0200

    fixed bugs

--- server.py
+++ server.py
@@ -2,20 +2,21 @@
 import socket
 import codecs
 import threading
+import time
 
 def send_offers(IP_address, port_num, server_udp_port_num, server_tcp_port_num):
     offer_counter = 0
     magic_cookie = "abcddcba"
     offer_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    offer_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     offer_socket.bind((IP_address, port_num))
-    message_to_send = codecs.encode(magic_cookie + 2 + server_udp_port_num + server_tcp_port_num, 'utf-8')
+    message_to_send = codecs.decode(magic_cookie, 'hex') + codecs.decode("02", 'hex') + codecs.decode(str(server_udp_port_num), 'hex') + codecs.decode(str((server_tcp_port_num)), 'hex')
     print(f"Server started, listening on IP address {IP_address}")
     while True:
         try:
             offer_socket.settimeout(1)
             offer_counter = offer_counter + 1
-            offer_socket.sendto(message_to_send, ("255.255.255.255", port_num))
-            offer_socket.listen(10)
+            offer_socket.sendto(message_to_send, ('255.255.255.255', port_num))
             offer_socket.settimeout(None)
         except socket.timeout:
             if offer_counter % 10 == 0:
@@ -25,12 +26,12 @@ def udp_connections(udp_socket, magic_cookie, port_number, server_IP_address):
     threads = []
     while True:
         (recievedmessage, address) = udp_socket.recvfrom(1024)
-        if recievedmessage[0:8].decode() != magic_cookie: 
+        if codecs.encode(recievedmessage[0:4], 'hex').hex() != magic_cookie: 
             print(f"Server recieved a request with the wrong cookie, from IP: {address[0]}")
-        elif int(recievedmessage[8:9].decode()) != 3:
+        elif int(codecs.encode(recievedmessage[0:4], 'hex').hex(), 16) != 3:
             print(f"Server recieved a request with the wrong message type, from IP: {address[0]}")
         else: #.
-            file_size = int(recievedmessage[9:17].decode())
+            file_size = int(codecs.encode(recievedmessage[0:4], 'hex').hex(), 16)
             print(f"Server received a request from {address[0]}")
             thread = threading.Thread(target=udp_connection, args=(file_size, magic_cookie, server_IP_address, address[0], address[1], port_number, ))
             thread.start()
@@ -40,8 +41,9 @@ def udp_connections(udp_socket, magic_cookie, port_number, server_IP_address):
 def tcp_connections(tcp_socket, port_number, server_IP_address):
     threads = []
     while True:
-        recievedmessage = tcp_socket.recv(1024)
-        (client_IP_address, client_port_nume) = tcp_socket.getpeername()
+        tcp_socket.listen(1000)
+        specific_tcp_socket, (client_IP_address, client_port_nume) = tcp_socket.accept()
+        recievedmessage = specific_tcp_socket.recv(1024)
         file_size = int(str(recievedmessage.decode())[:-1])
         print(f"Server received a request from client with IP: {client_IP_address}, and port number: {client_port_nume}")
         thread = threading.Thread(target=tcp_connection, args=(file_size, port_number, server_IP_address, client_IP_address, client_port_nume,))
@@ -56,8 +58,8 @@ def udp_connection(file_size, magic_cookie, server_IP_address, client_IP_address
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind((server_IP_address, port_number))
     while segment_counter < total_segments:
-        message_to_send = str(magic_cookie + 4 + total_segments + segment_counter)
-        message_to_send = message_to_send.ljust(segment_size, 'a')
+        message_to_send = codecs.decode(magic_cookie, 'hex') + codecs.decode("04", 'hex') + codecs.decode(str(total_segments), 'hex') + codecs.decode(str((segment_counter)), 'hex')
+        message_to_send = message_to_send + codecs.decode("".rjust(segment_size, 'aa'), 'hex')
         udp_socket.sendto(message_to_send, (client_IP_address, client_port_number))
         segment_counter = segment_counter + 1
         if segment_counter % 10 == 0:
@@ -85,6 +87,7 @@ if __name__ == '__main__':
     udp_connections_thread = threading.Thread(target=udp_connections, args=(udp_socket, magic_cookie, server_udp_port_num + 1000, IP_address))
     tcp_connections_thread = threading.Thread(target=tcp_connections, args=(tcp_socket, server_udp_port_num + 2000, IP_address))
     offer_thread.start()
+    time.sleep(1)
     udp_connections_thread.start()
     tcp_connections_thread.start()
 
